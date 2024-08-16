b8d2ed676b3b05c3f14ebc9eecf29fa057156304
Frankly <frankly.666@qq.com>
2024-08-16 16:45:42 +0800

    style: :art: 动态列表样式调整

--- src/views/community/cpns/moment/style.ts
+++ src/views/community/cpns/moment/style.ts
@@ -1,6 +1,13 @@
 import styled from "styled-components";
 
 const MomentWrapper = styled.div`
+  :where(.ant-list-item) {
+    &:hover {
+      cursor: pointer;
+      background: #f7f8fa;
+    }
+  }
+
   :where(.ant-list-item-meta-content) {
     text-align: left;
   }
