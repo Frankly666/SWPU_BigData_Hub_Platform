60741016b1b63ece5b5884628ae33166c7039ff7
Frankly <frankly.666@qq.com>
2024-08-26 09:44:48 +0800

    style: :art: 输入框组件hover动画

--- src/base_ui/editComments/index.tsx
+++ src/base_ui/editComments/index.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useState } from "react";
+import React, { memo, useRef, useState } from "react";
 import type { FC, ReactNode } from "react";
 
 import EditCommentsWrapper from "./style";
@@ -20,6 +20,9 @@ const EditComments: FC<IProps> = ({
   const [rightHeight, setRightHeight] = useState("0");
   const [border, setBorder] = useState("white");
   const [bgc, setBgc] = useState("#f2f3f5");
+  const [isListen, setIsListen] = useState(true);
+
+  const inputRef = useRef<HTMLInputElement>(null);
   return (
     <EditCommentsWrapper
       $minHeight={minHeight}
@@ -36,17 +39,29 @@ const EditComments: FC<IProps> = ({
           borderColor: border,
           backgroundColor: bgc
         }}
+        onClick={() => {
+          inputRef.current?.focus();
+        }}
+        onMouseEnter={() => {
+          if (isListen) setBgc("#e4e6eb");
+        }}
+        onMouseLeave={() => {
+          if (isListen) setBgc("#f2f3f5");
+        }}
       >
         <input
+          ref={inputRef}
           onFocus={() => {
             setRightHeight("200px");
             setBgc("white");
             setBorder("#1e80ff");
+            setIsListen(false);
           }}
           onBlur={() => {
             setRightHeight("0");
             setBgc("#f2f3f5");
             setBorder("white");
+            setIsListen(true);
           }}
           placeholder="友善交流"
         ></input>
